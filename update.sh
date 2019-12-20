49aef8a7be6b0798295ccede5eaf902f62814bb6
FranAmorosCubells <franacu@alumni.uv.es>
2019-12-20 08:34:19 +0100

    add function pkg install

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-pkg install git
+pkg_install()
+{
+    if [[ $(pkg list-installed | grep $1 | wc -l) -eq 1 ]]; then
+        pkg install $1
+    fi
+}
+
+pkg_install git
 git fetch
 pip install -upgrade youtube-dl &
 PID=$!
