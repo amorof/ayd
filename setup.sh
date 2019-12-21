03189f02882d86be77ac305d365fcfb37338e543
Francisco AC <bepitic@gmail.com>
2019-12-21 11:12:33 +0000

    re-building the script

--- setup.sh
+++ setup.sh
@@ -13,8 +13,68 @@ NC='\033[0m'
 
 #Debug x -> Display commands and arguments as they are executed.
 #Debug v -> Display input lines as they read.
-set -vx 
+set -vx
 
 # e -> If a command exits with an error exits.
 # u -> Treat unasigned variables as errors.
 set -eu
+
+pkg_install()
+{
+    #If it isn't intalled then intall
+    if [[ $(pkg list-installed 2>/dev/null| grep $1 | wc -l) -eq 0 ]]; then
+
+        #Launch in background stdout and stderr don't show, then get the PID
+        pkg install $1 -y 1>/dev/null 2>/dev/null &
+        INS_PID=$!
+
+        #See if the process it's running then do things
+        while kill -0 "$INS_PID" >/dev/null 2>&1; do
+          #play an animation while it's installing the program
+          echo -ne $GREEN "Installing (/)-> " $1 $NC "/r"
+          wait .3
+          echo -ne $GREEN "Installing (|)-> " $1 $NC "/r"
+          wait .3
+          echo -ne $GREEN "Installing (\)-> " $1 $NC "/r"
+          wait .3
+        done
+
+        #show when its installed
+        echo -ne $BLUE "Installed -----> " $1 $NC " <--/n"
+    fi
+}
+
+pip_install()
+{
+        #Launch in background stdout and stderr don't show, then get the PID
+        pip install --upgrade $1 -y 1>/dev/null 2>/dev/null &
+        INS_PID=$!
+
+        #See if the process it's running then do things
+        while kill -0 "$INS_PID" >/dev/null 2>&1; do
+          #play an animation while it's installing the program
+          echo -ne $GREEN "Installing (/)-> " $1 $NC "/r"
+          wait .3
+          echo -ne $GREEN "Installing (|)-> " $1 $NC "/r"
+          wait .3
+          echo -ne $GREEN "Installing (\)-> " $1 $NC "/r"
+          wait .3
+        done
+
+        #show when its installed
+        echo -ne $BLUE "Installed -----> " $1 $NC " <--/n"
+}
+
+termux-setup-storage &
+TSS_PID=$!
+
+pkg_install git
+pkg_install ffmpeg
+pkg_install python
+pip_install pip
+pip_install youtube-dl
+wait $TSS_PID
+rm -r $HOME/bin
+mkdir -p $HOME/bin
+WD_AYD=$(find $HOME -type d -name ayd )
+ln -s $HOME/bin/termux-url-opener $WD_AYD/tuo.sh
---
+++ tuo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo $@
+wait 20
