3bda85032ba407eb3fc6d50563dee3e39c167bf6
Francisco AC <bepitic@gmail.com>
2019-12-22 09:27:49 +0000

    implement improvements

--- tuo.sh
+++ tuo.sh
@@ -16,7 +16,6 @@ NC='\033[0m'
 #Debug x -> Display commands and arguments as they are executed.
 #Debug v -> Display input lines as they read.
 #set -vx
-set -v
 
 # e -> If a command exits with an error exits.
 # u -> Treat unasigned variables as errors.
